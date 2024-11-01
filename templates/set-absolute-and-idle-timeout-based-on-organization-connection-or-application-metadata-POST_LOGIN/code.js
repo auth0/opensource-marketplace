889522feaad912d5214ef2cdc31b91b6d1b9d132
Nelson Matias <nellson.maia@gmail.com>
2024-11-01 17:04:05 +0000

    Signed commit

--- templates/set-absolute-and-idle-timeout-based-on-organization-connection-or-application-metadata-POST_LOGIN/code.js
+++ templates/set-absolute-and-idle-timeout-based-on-organization-connection-or-application-metadata-POST_LOGIN/code.js
@@ -6,7 +6,7 @@ const IDLE_LIFETIME_FIELD = 'idle_lifetime';
 const ABSOLUTE_LIFETIME_FIELD = 'absolute_lifetime';
 
 /**
- * Handler that will be called during the execution of a PostLogin flow.
+ * Handler that will be called during the execution of a PostLogin flow
  *
  * @param {Event} event - Details about the user and the context in which they are logging in.
  * @param {PostLoginAPI} api - Interface whose methods can be used to change the behavior of the login.
