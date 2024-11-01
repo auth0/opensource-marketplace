45f53baf772605511b6f05168a9da07490bbc255
Nelson Matias <nellson.maia@gmail.com>
2024-11-01 15:58:32 +0000

    Running prettier

--- templates/set-absolute-and-idle-timeout-based-on-organization-connection-or-application-metadata-POST_LOGIN/code.js
+++ templates/set-absolute-and-idle-timeout-based-on-organization-connection-or-application-metadata-POST_LOGIN/code.js
@@ -12,28 +12,32 @@ const ABSOLUTE_LIFETIME_FIELD = 'absolute_lifetime';
  * @param {PostLoginAPI} api - Interface whose methods can be used to change the behavior of the login.
  */
 exports.onExecutePostLogin = async (event, api) => {
-  const currentTime = Date.now();
+    const currentTime = Date.now();
 
-  // Get organization, client, or connection metadata (organization takes precedence if present)
-  const metadata = event.organization?.metadata || event.client?.metadata || event.connection?.metadata;
+    // Get organization, client, or connection metadata (organization takes precedence if present)
+    const metadata =
+        event.organization?.metadata ||
+        event.client?.metadata ||
+        event.connection?.metadata;
 
-  if (metadata) {
-    // Set idle timeout using the metadata field defined in IDLE_LIFETIME_FIELD
-    const idleTimeout = Number(metadata[IDLE_LIFETIME_FIELD]);
-    if (idleTimeout) {
-      api.session.setIdleExpiresAt(currentTime + idleTimeout);
+    if (metadata) {
+        // Set idle timeout using the metadata field defined in IDLE_LIFETIME_FIELD
+        const idleTimeout = Number(metadata[IDLE_LIFETIME_FIELD]);
+        if (idleTimeout) {
+            api.session.setIdleExpiresAt(currentTime + idleTimeout);
+        }
+
+        // Set absolute timeout using the metadata field defined in ABSOLUTE_LIFETIME_FIELD
+        const absoluteTimeout = Number(metadata[ABSOLUTE_LIFETIME_FIELD]);
+        const createdAt = event.session?.created_at
+            ? new Date(event.session.created_at).getTime()
+            : null;
+
+        if (absoluteTimeout && createdAt) {
+            api.session.setExpiresAt(createdAt + absoluteTimeout);
+        }
     }
-
-    // Set absolute timeout using the metadata field defined in ABSOLUTE_LIFETIME_FIELD
-    const absoluteTimeout = Number(metadata[ABSOLUTE_LIFETIME_FIELD]);
-    const createdAt = event.session?.created_at ? new Date(event.session.created_at).getTime() : null;
-
-    if (absoluteTimeout && createdAt) {
-      api.session.setExpiresAt(createdAt + absoluteTimeout);
-    }
-  }
 };
-
 
 /**
  * Handler that will be invoked when this action is resuming after an external redirect. If your
